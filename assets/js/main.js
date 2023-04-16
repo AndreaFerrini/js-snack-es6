32e909e8d790b0e9ce723ded5bb49a9e5ad06cbb
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-04-16 22:53:47 +0200

    inserito snack 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,40 @@ Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengo
 Snack 3
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 l'array egnerato da parte vostra dovrà essere di ad esempio nomi o cose: ['alberto', 'michele','angelo','simone']
-se come numeri ad esemio ho 2 e 4 devo inserire in un array nuovo l'elemento in posizione 3
+se come numeri ad esempio ho 2 e 4 devo inserire in un array nuovo l'elemento in posizione 3
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 Usiamo i nuovi metodi degli array foreach o filter.
 */
 
+
+/* snack 1 */
+
+const arrayBici = [
+
+    {
+      nome : "Bianchi",
+      peso : 10
+    },
+  
+    {
+      nome : "Tomassini",
+      peso : 15
+    },
+  
+    {
+      nome : "Atala",
+      peso : 5
+    },
+  
+    {
+      nome : "Graziella",
+      peso : 3
+    }
+  
+]
+
+const { nome, peso } = arrayBici[3]
+  
+  console.log( nome, peso )
+  
+  document.getElementById("biciLight").innerHTML = `La bici più leggera è: ${nome} con un peso di ${peso}kg `
